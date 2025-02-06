063410d4731926ee1a07ef30c4b73537b48257ed
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2025-02-06 20:49:17 +0800

    Refactor code for improved performance

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const container = document.querySelector(".container");
+const fragmentDivs = document.createDocumentFragment();
 
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const div = document.createElement("div");
         div.className = `div-${i}-${j}`;
-        div.style.height = "50px";
-        div.style.width = "50px";
+        div.style.height = "25px";
+        div.style.width = "25px";
         div.style.border = "1px solid black";
 
-        container.appendChild(div);
+        fragmentDivs.appendChild(div);
     }
 }
 
-console.log(container);
+container.appendChild(fragmentDivs);
