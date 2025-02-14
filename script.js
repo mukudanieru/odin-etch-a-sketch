e3a3c3382a1a79a4a76224dbd5ae4ecf2da7e210
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2025-02-14 11:07:20 +0800

    Refactor code by modularizing processes into functions

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 const container = document.querySelector(".container");
 const fragmentDivs = document.createDocumentFragment();
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const div = document.createElement("div");
-        div.className = `div-${i}-${j}`;
-        div.style.height = "25px";
-        div.style.width = "25px";
-        div.style.border = "1px solid black";
-
-        fragmentDivs.appendChild(div);
-    }
-}
-
-container.appendChild(fragmentDivs);
-
-function hoverEffect(event) {
-    if (event.target !== container) {
-        event.stopPropagation();
-        console.log(event.target.className);
-        event.target.style.backgroundColor = "#0e0e55";
-    }
-}
-
-container.addEventListener("mouseenter", hoverEffect, {
-    capture: true,
+// MAIN
+document.addEventListener("DOMContentLoaded", () => {
+    createGrid(16, 16);
+    addHoverEffect();
 });
+
+function createGrid(width, height) {
+    for (let i = 0; i < 16; i++) {
+        for (let j = 0; j < 16; j++) {
+            const div = document.createElement("div");
+            div.className = `div-${i}-${j}`;
+            div.style.height = "25px";
+            div.style.width = "25px";
+            div.style.border = "1px solid black";
+
+            fragmentDivs.appendChild(div);
+        }
+    }
+
+    container.appendChild(fragmentDivs);
+}
+
+function addHoverEffect() {
+    function hoverEffect(event) {
+        if (event.target !== container) {
+            event.stopPropagation();
+            console.log(event.target.className);
+            event.target.style.backgroundColor = "#0e0e55";
+        }
+    }
+
+    container.addEventListener("mouseenter", hoverEffect, {
+        capture: true,
+    });
+}
